edae3c76f169ebbbf4c9a9c0f71e868bc7bd03c7
Womack <D_wom3@yahoo.com>
2022-06-24 11:18:19 -0400

    created nav bar

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let url = `https://rickandmortyapi.com/api`
+const url = `https://rickandmortyapi.com/api`
+const char = `https://rickandmortyapi.com/api/character`
+const location = `https://rickandmortyapi.com/api/location`
+const episode = `https://rickandmortyapi.com/api/episode`
+
 
 fetch('https://rickandmortyapi.com/api')
   .then(res => res.json())
