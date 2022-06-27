fd9a0734f431fef05a6cc8637013f2c981d4e3b0
Womack <D_wom3@yahoo.com>
2022-06-27 15:47:53 -0400

    Modal complete, Added previous and next button. Edited Nav Bar and a few CSS features

--- script.js
+++ script.js
@@ -1,47 +1,110 @@
+// -----------------------------
+//       Fetching API
+// -----------------------------
 
-const menu = document.querySelector(".menu")
-const navMenu = document.querySelector(".nav-menu")
+
 const url = `https://rickandmortyapi.com/api`
 const char = `https://rickandmortyapi.com/api/character`
 const episode = `https://rickandmortyapi.com/api/episode`
+
+const menu = document.querySelector(".menu")
+const navMenu = document.querySelector(".nav-menu")
 const divImg = document.querySelectorAll('.div-img')
-
 const imgContainer = document.querySelector('.img-container')
-const images = [];
 
+
+// -----------------------------
+//       Prev/Next Buttons
+// -----------------------------
+
+// 
+const nextButton = document.querySelector('#next')
+const prevButton = document.querySelector('#prev')
+let next = null;
+let prev = null;
 
 fetch(char)
-  .then(res => res.json())
-  .then(data => { 
-    for (let i = 0; i < 20; i++) {
-      let info = `
-      <div class="img-div">
-          <h3>${data.results[i].name}</h3>
-          <h3>${data.results[i].status}</h3>
-        <img class="div-img" src="${data.results[i].image}">
-      </div>`
-      imgContainer.insertAdjacentHTML('beforeend', info)
-      console.log(data)
-    }
+  .then((res) => res.json())
+  .then((data) => {
+    next = data.info.next;
+    prev = data.info.prev;
+    displayData(data);
+  });
+
+  function displayData(data) {
+    imgContainer.innerHTML = '';
     
-    const imgDivs = document.querySelectorAll('.img-div')
+    for (let i = 0; i < data.results.length; i++) {
+      let info = `
+      <div class='img-div'>
+      <h3>${data.results[i].name}</h3>
+      <img class ='div-img' src='${data.results[i].image}'>
+      </div>`;
+      imgContainer.insertAdjacentHTML('beforeend', info)
+    }
+    const imgDivs = document.querySelectorAll('img-div');
+    
     imgDivs.forEach((imgDiv, i) => {
-      imgDiv.addEventListener('click', () => xtraInfo(data.results[i]))
-    })
-  })
-
-function xtraInfo(data) {
-    console.log(data)
+      imgDiv.addEventListener('click', () => xtraInfo(data.results[i]));
+    });
   }
+  
+  // -----------------------------
+//            Modal
+// -----------------------------
 
+// Get the modal
+let modal = document.getElementById('myModal');
 
-let handleRezise = () => {
+//Closing the modal 
+window.onclick = function (e) {
+  if (e.target == modal) {
+    modal.style.display = 'none'
+  }
+};
 
+//Modal Content
+function xtraInfo(data) {
+  console.log(data);
+  //Display modal
+  modal.style.display = 'block';
+  //Clear previous modal
+  modal.innerHTML = '';
+  
+  //Setting Modal's Content
+  //Grabbing Name, Image, Origin Location, Species and Status
+  let info = `
+  <div class='modal-content'>
+  <span class='close'>&times;</span>
+  <h2>${data.name}<h2> 
+  <img class='div-img' src='${data.image}'>
+  <p${data.origin.name}</p>
+  <p${data.species}</p>
+  <p${data.status}</p>
+  
+  </div>`
+  
+  //Injecting Modal Content
+  modal.insertAdjacentHTML('beforeend', info);
+  // Grabbing the <span> to close modal
+  let span = document.getElementsByClassName('close')[0];
+  //When user clicks <span> (x), closes modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  }
+}
+
+// -----------------------------
+//       Hamburger Menu
+// -----------------------------
+
+let handleResize = () => {
+  
   menu.addEventListener("click", () => {
     menu.classList.toggle("active");
     navMenu.classList.toggle("active");
   })
-
+  
   document.querySelectorAll(".nav-link").forEach(e => e.addEventListener("click", () => {
     menu.classList.remove("active");
     navMenu.classList.remove("active");
@@ -50,84 +113,27 @@ let handleRezise = () => {
     return window.innerWidth
   }
 }
-window.addEventListener("resize", handleRezise)
+window.addEventListener("resize", handleResize)
+handleResize();
 
-// Model Content
-function xtraInfo(data) {
-  console.log(data);
-  // Display model
-  modal.style.display = "block";
+// Next Button
+nextButton.addEventListener("click", () => {
+fetch(next)
+  .then((res) => res.json())
+  .then((data) => {
+    next = data.info.next;
+    prev = data.info.prev;
+    displayData(data);
+  });
+});
 
-  // Clear previous modal content
-  modal.innerHTML = "";
-
-  // set model content
-  let info = `
-    <div class="modal-content">
-    <span class="close">&times;</span>
-    <h2>${data.name}</h2>
-    <img class="div-img" src="${data.image}">
-    <p>${data.origin.name}</p>
-    <p>${data.species}</p>
-    <p>${data.status}</p>
-    </div>
-    `;
-
-  // Inject model content
-  modal.insertAdjacentHTML("beforeend", info);
-
-  // Get the <span> element that closes the modal
-  let span = document.getElementsByClassName("close")[0];
-
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function () {
-    modal.style.display = "none";
-  };
-}
-
-
-// let i = 0;
-// const time = 2000;
-// const width = 1000;
-// let delta = width;
-
-// async function fillImages() {
-//   const url = `https://rickandmortyapi.com/api/character`
-//   for (let i = 0; i < 12; i++) {
-//     let response = await fetch(url)
-//     let data = await response.json();
-
-
-//     images.push(data.value)
-//   }
-
-//   renderData()
-// }
-
-// fillImages()
-
-// function renderData() {
-//   let imagesTags = ""
-
-//   for (let i = 0; i < imgDiv.length; i++) {
-//     imagesTags += `<div class="img-div">${images[i]}</div>`
-//   }
-
-//   imgDiv.insertAdjacentHTML('beforeend', imagesTags)
-// }
-
-// function changeImages() {
-//   if (i < images.length - 1) {
-//     imgDiv.style.transform = `translateX(${-delta}px)`
-//     delta += width
-//     i++
-//   } else {
-//     imgDiv.style.transform = `translateX(0px)`
-//     delta = 0
-//     i = 0
-//   }
-// }
-
-// setInterval(changeImages, time)
-
-
+// Prev Button
+prevButton.addEventListener("click", () => {
+fetch(prev)
+  .then((res) => res.json())
+  .then((data) => {
+    next = data.info.next;
+    prev = data.info.prev;
+    displayData(data);
+  });
+});
